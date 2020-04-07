5a6313127190bd5bd6034a58eaeee9e373816277
Charlison Fideles <charlin.pg@hotmail.com>
2020-04-07 18:47:35 -0300

    trabalhando com a biblioteca axios

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://api.github.com/users/charlison');
-xhr.send(null);
-
-xhr.onreadstatechange = function(){
-    if(xhr.readState === 4){
-        console.log(JSON.parse(xhr.responseText));
-    }
-}
+axios.get('https://api.github.com/users/charlison')
+    .then(function(response){
+        console.log(response);
+    })
+    .catch(function(error){
+        console.warn(error);
+    })
